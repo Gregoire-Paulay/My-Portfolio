6db7500544ed296b67b9d6282ebfb41bdca18ae4
Grégoire Paulay <gregoirepaulay.pro@gmail;com>
2023-08-27 17:32:55 +0200

    Page projets en cours

--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -1,7 +1,111 @@
+import tripadvisor from "../assets/tripadvisor.png";
+import vinted from "../assets/vinted.png";
+import deliveroo from "../assets/deliveroo.png";
+
 const Projects = ({ darkMode }) => {
   return (
     <main className={darkMode ? "dark" : "light"}>
-      <div className="container">Je suis sur mes projets</div>
+      <div className="container">
+        <div className="my-Project">
+          <h2>Mes projets</h2>
+          <p>
+            Voici une liste des projets réalisé au cours de ma formation, ainsi
+            que les projets à venir
+          </p>
+          <h3>Projets terminés</h3>
+          <section className="finished">
+            <div>
+              <a href="https://tripadvisor-gp.netlify.app">
+                <img src={tripadvisor} alt="tripadvisor" />
+              </a>
+
+              <div>
+                <h4>Tripadvisor Web</h4>
+                <h5>Projet Front-End</h5>
+                <p>
+                  <i className="fa-solid fa-square"></i>Envoi automatisé de
+                  mails
+                </p>
+                <p>
+                  <i className="fa-solid fa-square"></i>Carousel de photos
+                </p>
+                <p className="github">
+                  <i className="fa-brands fa-github"></i>
+                  <a href="https://github.com/Gregoire-Paulay/Tripadvisor-Integration">
+                    Répertoire Github
+                  </a>
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <a href="https://deliveroo-gp.netlify.app">
+                <img src={deliveroo} alt="deliveroo" />
+              </a>
+              <div>
+                <h4>Deliveroo Web</h4>
+                <h5>Projet Front-End</h5>
+                <p>
+                  <i className="fa-solid fa-square"></i>Récupération de données
+                </p>
+                <p>
+                  <i className="fa-solid fa-square"></i>Panier de commande
+                </p>
+                <p className="github">
+                  <i className="fa-brands fa-github"></i>
+                  <a href="https://github.com/Gregoire-Paulay/Deliveroo-frontend">
+                    Répertoire Github
+                  </a>
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <h3>Projets en cours</h3>
+          <section className="building">
+            <div>
+              <a href="https://deliveroo-gp.netlify.app">
+                <img src={vinted} alt="vinted" />
+              </a>
+              <div>
+                <h4>Vinted Web</h4>
+                <h5>Projet Full-Stack</h5>
+                <p>
+                  <i className="fa-solid fa-square"></i>Inscription / Connexion
+                </p>
+                <p>
+                  <i className="fa-solid fa-square"></i>Récupération de données
+                </p>
+                <p>
+                  <i className="fa-solid fa-square"></i>Publication d'annonces
+                </p>
+                <p>
+                  <i className="fa-solid fa-square"></i>Barre de recherche
+                </p>
+                <p>
+                  <i className="fa-solid fa-square"></i>Paiement
+                </p>
+                <p>
+                  <i className="fa-solid fa-square"></i>Upload de Photo
+                </p>
+                <p className="github">
+                  <i className="fa-brands fa-github"></i>
+                  <a href="https://github.com/Gregoire-Paulay/Deliveroo-frontend">
+                    Répertoire Github Front-End
+                  </a>
+                </p>
+                <p className="github">
+                  <i className="fa-brands fa-github"></i>
+                  <a href="https://github.com/Gregoire-Paulay/Deliveroo-frontend">
+                    Répertoire Github Back-End
+                  </a>
+                </p>
+              </div>
+            </div>
+          </section>
+          <section className="coming-soon"></section>
+        </div>
+      </div>
     </main>
   );
 };
